1ea5ab87005a38b9b3e8ba63c7b4159a4b2fee56
vidhyagowda10 <vidhyagowdads10@gmail.com>
2026-05-05 07:49:50 +0530

    Refactor arithmetic operations and fix spelling
    Fixed indentation and corrected spelling of 'Floar' to 'Floor'.

--- p.02.py
+++ p.02.py
@@ -7,3 +7,13 @@ print("Division of a and b ",a/b)
 print("Remainder of a and b ",a%b)
 print("Exponent of a and b ",a**b)
 print("Floar division of a and b ",a//b)
+output:
+      Enter a value2
+Enter b value3
+Addition of a and b  5
+Subtraction of a and b  -1
+Multiplication of a and b  6
+Division of a and b  0.6666666666666666
+Remainder of a and b  2
+Exponent of a and b  8
+Floar division of a and b  0
