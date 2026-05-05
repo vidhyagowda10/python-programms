67e3f2aa94eda9cf1b30f77217cf9d45d6cb1624
vidhyagowda10 <vidhyagowdads10@gmail.com>
2026-05-05 07:49:11 +0530

    Add arithmetic operations after prime number output
    Added arithmetic operations and their outputs after prime number calculation.

--- p.010.py
+++ p.010.py
@@ -8,3 +8,14 @@ for num in range(ulmt):
                break
        else:
            print(num)
+          output:
+      Enter a value2
+Enter b value3
+Addition of a and b  5
+Subtraction of a and b  -1
+Multiplication of a and b  6
+Division of a and b  0.6666666666666666
+Remainder of a and b  2
+Exponent of a and b  8
+Floar division of a and b  0
+
