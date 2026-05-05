2d6591cdc9fee87e9c05e488120bb3841f78fe25
vidhyagowda10 <vidhyagowdads10@gmail.com>
2026-05-05 07:53:44 +0530

    Fix pets list and add output print statements

--- p.04 pets = ['cat', 'dog', 'rat', 'pig', 'tig.py
+++ p.04 pets = ['cat', 'dog', 'rat', 'pig', 'tig.py
@@ -6,5 +6,10 @@ animals=pets+snakes
 print("Animals are :",animals)
 snakes.remove("fish")
 print("updated Snakes are :",snakes) 
+output:
+Pets are : ['cat', 'dog', 'rat', 'pig', 'tiger']
+Snakes are : ['python', 'anaconda', 'fish', 'cobra', 'mamba']
+Animals are : ['cat', 'dog', 'rat', 'pig', 'tiger', 'python', 'anaconda', 'fish', 'cobra', 'mamba']
+updated Snakes are : ['python', 'anaconda', 'cobra', 'mamba']
 
 
