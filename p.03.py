2450e91a0ca33e9399ea22df5f44a2c0139f4004
vidhyagowda10 <vidhyagowdads10@gmail.com>
2026-05-05 07:52:26 +0530

    Update p.03.py

--- p.03.py
+++ p.03.py
@@ -6,4 +6,11 @@ print("Second string is :", s2)
 
 print("Concatenation of two strings :", s1 + s2)
 print("Substring of given string :", s1[1:4])
+output:
+Enter first String : 2
+Enter second String : 4
+First string is : 2
+Second string is : 4
+Concatenation of two strings : 24
+Substring of given string : 
 
